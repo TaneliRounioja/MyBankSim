e78980f87bb13b2840b3d959dd2e0b2675672e5e
Taneli Rounioja <trounioja@gmail.com>
2024-04-09 00:04:41 +0300

    account_balance kohdat -> balance

--- backend/models/account.model.js
+++ backend/models/account.model.js
@@ -8,13 +8,13 @@ const account={
         return db.query("SELECT * FROM account WHERE idcustomer=?", [id], callback);
     },
     getBalance(id, callback){
-        return db.query("SELECT account_balance FROM account WHERE idcustomer=?", [id], callback);
+        return db.query("SELECT balance FROM account WHERE idcustomer=?", [id], callback);
     },
     addAccount(newAccount, callback){
-        return db.query("INSERT INTO account(fname, account_balance, idcustomer) VALUES(?,?,?)", [newAccount.fname, newAccount.account_balance. newAccount.idcustomer],callback);
+        return db.query("INSERT INTO account(fname, balance, idcustomer) VALUES(?,?,?)", [newAccount.fname, newAccount.balance. newAccount.idcustomer],callback);
     },
     updateAccount(id, updateAccount,callback){
-        return db.query("UPDATE account SET fname=?, account_balance=?, idcustomer=? WHERE idaccount=?", [updateAccount.fname, updateAccount.account_balance, updateAccount.idcustomer, id], callback)
+        return db.query("UPDATE account SET fname=?, balance=?, idcustomer=? WHERE idaccount=?", [updateAccount.fname, updateAccount.balance, updateAccount.idcustomer, id], callback)
     },
     deleteAccount(id,callback){
         return db.query("DELETE FROM account WHERE idaccount=?", [id], callback);
