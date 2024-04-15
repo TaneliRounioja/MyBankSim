33881d40dffab2931e89a5d6582c58b1e7b47e71
Joel Salonen <sjogeee@gmail.com>
2024-04-15 17:08:41 +0100

    Route update

--- backend/routes/transaction.js
+++ backend/routes/transaction.js
@@ -8,13 +8,14 @@ router.get('/', function(request, response) {
     transaction.getAllTransactions(function(err, result) {
         if (err) {
             console.error("Error retrieving transactions:", err);
-            response.status(500).json({ error: "Failed to retrieve transactions" });
+            response.status(500).json({ error: "Failed to retrieve transactions", errorMessage: err.message });
         } else {
             console.log("Transactions retrieved successfully:", result);
-            response.status(200).json(result);
+            response.status(200).json({ Transactions: result });
         }
     });
 });
+
 
 router.get('/:id', function(request, response) {
     const accountId = request.params.id;
@@ -22,11 +23,11 @@ router.get('/:id', function(request, response) {
     transaction.getTransactionsByAccount(accountId, function(err, result) {
         if (err) {
             console.error("Error retrieving transactions for account ID:", accountId, err);
-            response.status(500).json({ error: "Failed to retrieve transactions" });
+            response.status(500).json({ error: "Failed to retrieve transactions", errorMessage: err.message });
         } else {
             console.log("Transactions retrieved successfully for account ID:", accountId);
             console.log("Transactions:", result);
-            response.status(200).json(result);
+            response.status(200).json({ Transactions: result });
         }
     });
 });
@@ -60,6 +61,7 @@ router.put('/:id', (request, response) => {
             response.status(500).json({ error: "Failed to update transaction" });
         } else {
             if (result.affectedRows === 0) {
+                console.log("Transaction not found or already updated");
                 response.status(404).json({ message: "Transaction not found or already updated" });
             } else {
                 console.log("Transaction updated successfully");
@@ -77,6 +79,7 @@ router.delete('/:id', function(request, response) {
             response.status(500).json({ error: "Failed to delete transaction" });
         } else {
             if (result.affectedRows === 0) {
+                console.log("Transaction not found or already deleted");
                 response.status(404).json({ message: "Transaction not found or already deleted" });
             } else {
                 console.log("Transaction deleted successfully");
