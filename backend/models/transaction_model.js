49065d0adb4a9081cb54c2611ea487a6eba543f5
Joel Salonen <sjogeee@gmail.com>
2024-04-15 12:36:29 +0100

    Model update

--- backend/models/transaction_model.js
+++ backend/models/transaction_model.js
@@ -8,15 +8,17 @@ const transaction = {
         return db.query("SELECT * FROM transaction WHERE idaccount=?", [id], callback);
     },
     addTransaction(newTransaction, callback){   // Adds a new transaction to the database //
-        return db.query("INSERT INTO transaction(idaccount, transaction_date, transaction, amount, account_number) VALUES(?,?,?,?,?",
+        return db.query("INSERT INTO transaction(idaccount, transaction_date, transaction, amount, account_number) VALUES(?,?,?,?,?)",
             [newTransaction.idaccount, newTransaction.transaction_date, newTransaction.transaction, newTransaction.amount, newTransaction.account_number], callback);
     },
-    updateTransaction(id, updateTransaction, callback) {    // Updates an existing transaction in the database //
-        return db.query("UPDATE transaction SET idaccount=?, transaction_date=?, transaction=?, amount=?, account_number=? WHERE idTransaction=?", 
-            [updateTransaction.idaccount, updateTransaction.transaction_date, updateTransaction.transaction, updateTransaction.amount, updateTransaction.account_number, id], callback);
+    updateTransaction(id, updateTransaction, callback) {    // Updates an existing transaction in the database
+        return db.query(
+            "UPDATE transaction SET idaccount=NULL, transaction_date=NULL, transaction=NULL, amount=NULL, Account_Number=NULL WHERE idAccount=?",
+            [updateTransaction.idaccount, updateTransaction.transaction_date, updateTransaction.transaction, updateTransaction.amount, updateTransaction.account_number, id],
+            callback);
     },
-    deleteTransaction(id, callback) {   // Deletes a transaction from the database //
-        return db.query("DELETE FROM transaction WHERE idTransaction=?", [id], callback);
+    deleteTransaction(id, callback) {
+        return db.query("DELETE FROM transaction WHERE idAccount =1",[id], callback);
     }
 }
 
