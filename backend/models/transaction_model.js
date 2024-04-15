6439f056c109de7da874cb2f040f0a2a6ba283da
Joel Salonen <sjogeee@gmail.com>
2024-04-15 17:21:19 +0100

    Model update

--- backend/models/transaction_model.js
+++ backend/models/transaction_model.js
@@ -1,17 +1,17 @@
 const db=require('../database');
 
 const transaction = {   
-    getAllTransactions(callback){   // Retrieves all transactions from the database //
+    getAllTransactions(callback){ 
         return db.query("SELECT * FROM transaction", callback);
     },
-    getTransactionsByAccount(id, callback){     // Retrieves transactions associated with a specific amount //
+    getTransactionsByAccount(id, callback){   
         return db.query("SELECT * FROM transaction WHERE idaccount=?", [id], callback);
     },
-    addTransaction(newTransaction, callback){   // Adds a new transaction to the database //
+    addTransaction(newTransaction, callback){  
         return db.query("INSERT INTO transaction(idaccount, transaction_date, transaction, amount, account_number) VALUES(?,?,?,?,?)",
             [newTransaction.idaccount, newTransaction.transaction_date, newTransaction.transaction, newTransaction.amount, newTransaction.account_number], callback);
     },
-    updateTransaction(id, updateTransaction, callback) {    // Updates an existing transaction in the database
+    updateTransaction(id, updateTransaction, callback) {    
         return db.query(
             "UPDATE transaction SET idaccount=NULL, transaction_date=NULL, transaction=NULL, amount=NULL, Account_Number=NULL WHERE idAccount=?",
             [updateTransaction.idaccount, updateTransaction.transaction_date, updateTransaction.transaction, updateTransaction.amount, updateTransaction.account_number, id],
