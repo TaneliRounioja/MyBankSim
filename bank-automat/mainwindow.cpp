3ed91e7cfedda33234cd1be8a78b7fe5493e198e
Taneli Rounioja <trounioja@gmail.com>
2024-04-24 16:15:24 +0300

    login toimii QT:ssä

--- bank-automat/mainwindow.cpp
+++ bank-automat/mainwindow.cpp
@@ -1,3 +1,4 @@
+#include "environment.h"
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
@@ -12,3 +13,30 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_btnLogin_clicked()
+{
+    QString uname=ui->textUsername->text();
+    QString passwd=ui->textPassword->text();
+    QJsonObject jsonObj;
+    jsonObj.insert("idCard",uname);
+    jsonObj.insert("pin",passwd);
+
+    QString site_url=Environment::getBaseUrl()+"/login";
+    QNetworkRequest request((site_url));
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+
+    loginManager = new QNetworkAccessManager(this);
+    connect(loginManager, SIGNAL(finished (QNetworkReply*)), this, SLOT(loginSlot(QNetworkReply*)));
+
+    reply = loginManager->post(request, QJsonDocument(jsonObj).toJson());
+}
+
+void MainWindow::loginSlot(QNetworkReply *reply)
+{
+    response_data=reply->readAll();
+    qDebug()<<response_data;
+
+
+}
+
